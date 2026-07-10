31c806d7204221b3f2c89eb9c718833ba098ef43
ggulverse <bbangnabal@gmail.com>
2026-07-10 20:26:08 +0900

    Update bmi.js

--- js/bmi.js
+++ js/bmi.js
@@ -20,11 +20,15 @@ calculateButton.addEventListener(
         Number(document.getElementById("weight").value);
 
 
+        const result =
+        document.getElementById("result");
+
+
 
         if(!height || !weight){
 
-            document.getElementById("result").innerHTML =
-            "키와 몸무게를 입력해주세요.";
+            result.innerHTML =
+            "키와 몸무게를 모두 입력해주세요.";
 
             return;
 
@@ -42,39 +46,78 @@ calculateButton.addEventListener(
 
 
 
-        let category = "";
+        let whoCategory = "";
+        let asiaCategory = "";
 
 
+
+        // WHO 기준
 
         if(bmi < 18.5){
 
-            category = "저체중";
+            whoCategory = "저체중";
 
         }
         else if(bmi < 25){
 
-            category = "정상";
+            whoCategory = "정상";
 
         }
         else if(bmi < 30){
 
-            category = "과체중";
+            whoCategory = "과체중";
 
         }
         else{
 
-            category = "비만";
+            whoCategory = "비만";
 
         }
 
 
 
-        document.getElementById("result").innerHTML =
+        // 아시아 기준
+
+        if(bmi < 18.5){
+
+            asiaCategory = "저체중";
+
+        }
+        else if(bmi < 23){
+
+            asiaCategory = "정상";
+
+        }
+        else if(bmi < 25){
+
+            asiaCategory = "위험체중";
+
+        }
+        else{
+
+            asiaCategory = "비만 위험 증가";
+
+        }
+
+
+
+        result.innerHTML =
 
         `
-        BMI : ${bmi.toFixed(1)}<br><br>
+        BMI : ${bmi.toFixed(1)}
+        <br><br>
 
-        판정 : ${category}
+        WHO 기준 : ${whoCategory}
+        <br>
+
+        아시아 기준 : ${asiaCategory}
+
+        <br><br>
+
+        <small>
+        BMI는 성인의 일반적인 체중 상태를 확인하는 지표이며,
+        근육량과 체형에 따라 실제 건강 상태와 차이가 있을 수 있습니다.
+        </small>
         `;
 
 
