354882ca6783cdb6b17e0d32f6a62de99b4ca9b5
ggulverse <bbangnabal@gmail.com>
2026-07-11 20:42:42 +0900

    Create metals.js

---
+++ js/metals.js
@@ -0,0 +1,36 @@
+/* ==================================================
+   EveryCalc - metals.js
+   Precious Metals Data
+================================================== */
+
+
+const metals = [
+
+    {
+        code: "XAU",
+        name: "금",
+        symbol: "🟡"
+    },
+
+
+    {
+        code: "XAG",
+        name: "은",
+        symbol: "⚪"
+    },
+
+
+    {
+        code: "XPT",
+        name: "백금",
+        symbol: "⚪"
+    },
+
+
+    {
+        code: "XPD",
+        name: "팔라듐",
+        symbol: "⚪"
+    }
+
+];
