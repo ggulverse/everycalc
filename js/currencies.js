0c5fc8f64f10e17a4d3a88067927c8556a4eed28
ggulverse <bbangnabal@gmail.com>
2026-07-11 10:42:18 +0900

    Update currencies.js

--- js/currencies.js
+++ js/currencies.js
@@ -9,172 +9,144 @@ const currencies = [
     {
         code:"KRW",
         name:"대한민국 원",
-        flag:"🇰🇷",
         type:"currency"
     },
 
     {
         code:"USD",
         name:"미국 달러",
-        flag:"🇺🇸",
         type:"currency"
     },
 
     {
         code:"EUR",
         name:"유로",
-        flag:"🇪🇺",
         type:"currency"
     },
 
     {
         code:"JPY",
         name:"일본 엔",
-        flag:"🇯🇵",
         type:"currency"
     },
 
     {
         code:"CNY",
         name:"중국 위안",
-        flag:"🇨🇳",
         type:"currency"
     },
 
     {
         code:"GBP",
         name:"영국 파운드",
-        flag:"🇬🇧",
         type:"currency"
     },
 
     {
         code:"AUD",
         name:"호주 달러",
-        flag:"🇦🇺",
         type:"currency"
     },
 
     {
         code:"CAD",
         name:"캐나다 달러",
-        flag:"🇨🇦",
         type:"currency"
     },
 
     {
         code:"CHF",
         name:"스위스 프랑",
-        flag:"🇨🇭",
         type:"currency"
     },
-
 
     {
         code:"HKD",
         name:"홍콩 달러",
-        flag:"🇭🇰",
         type:"currency"
     },
 
     {
         code:"SGD",
         name:"싱가포르 달러",
-        flag:"🇸🇬",
         type:"currency"
     },
 
     {
         code:"THB",
         name:"태국 바트",
-        flag:"🇹🇭",
         type:"currency"
     },
 
     {
         code:"VND",
         name:"베트남 동",
-        flag:"🇻🇳",
         type:"currency"
     },
 
     {
         code:"TWD",
         name:"대만 달러",
-        flag:"🇹🇼",
         type:"currency"
     },
 
     {
         code:"INR",
         name:"인도 루피",
-        flag:"🇮🇳",
         type:"currency"
     },
 
     {
         code:"IDR",
         name:"인도네시아 루피아",
-        flag:"🇮🇩",
         type:"currency"
     },
 
     {
         code:"MYR",
         name:"말레이시아 링깃",
-        flag:"🇲🇾",
         type:"currency"
     },
 
     {
         code:"PHP",
         name:"필리핀 페소",
-        flag:"🇵🇭",
         type:"currency"
     },
-
 
     {
         code:"AED",
         name:"아랍에미리트 디르함",
-        flag:"🇦🇪",
         type:"currency"
     },
 
     {
         code:"SAR",
         name:"사우디 리얄",
-        flag:"🇸🇦",
         type:"currency"
     },
-
 
     {
         code:"TRY",
         name:"터키 리라",
-        flag:"🇹🇷",
         type:"currency"
     },
 
     {
         code:"RUB",
         name:"러시아 루블",
-        flag:"🇷🇺",
         type:"currency"
     },
 
     {
         code:"BRL",
         name:"브라질 헤알",
-        flag:"🇧🇷",
         type:"currency"
     },
-
 
     {
         code:"XAU",
         name:"금 (1 트로이온스)",
-        flag:"🪙",
         type:"metal",
         unit:"31.1035g"
     },
@@ -182,7 +154,6 @@ const currencies = [
     {
         code:"XAG",
         name:"은 (1 트로이온스)",
-        flag:"🥈",
         type:"metal",
         unit:"31.1035g"
     }
