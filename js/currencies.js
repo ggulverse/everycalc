5d37a7c3361c1385196022e97aa37e7ce52bd041
ggulverse <bbangnabal@gmail.com>
2026-07-11 11:18:23 +0900

    Update currencies.js

--- js/currencies.js
+++ js/currencies.js
@@ -1,4 +1,4 @@
-/* ==================================================
+ /* ==================================================
    EveryCalc - currencies.js
    Currency Information
 ================================================== */
@@ -6,184 +6,208 @@
 
 const currencies = [
 
-    {
-        code:"KRW",
-        name:"대한민국 원",
-        icon:"flags/kr.svg",
-        type:"currency"
-    },
 
-    {
-        code:"USD",
-        name:"미국 달러",
-        icon:"flags/us.svg",
-        type:"currency"
-    },
-
-    {
-        code:"EUR",
-        name:"유로",
-        icon:"flags/eu.svg",
-        type:"currency"
-    },
-
-    {
-        code:"JPY",
-        name:"일본 엔",
-        icon:"flags/jp.svg",
-        type:"currency"
-    },
-
-    {
-        code:"CNY",
-        name:"중국 위안",
-        icon:"flags/cn.svg",
-        type:"currency"
-    },
-
-    {
-        code:"GBP",
-        name:"영국 파운드",
-        icon:"flags/gb.svg",
-        type:"currency"
-    },
-
-    {
-        code:"AUD",
-        name:"호주 달러",
-        icon:"flags/au.svg",
-        type:"currency"
-    },
-
-    {
-        code:"CAD",
-        name:"캐나다 달러",
-        icon:"flags/ca.svg",
-        type:"currency"
-    },
-
-    {
-        code:"CHF",
-        name:"스위스 프랑",
-        icon:"flags/ch.svg",
-        type:"currency"
-    },
-
-    {
-        code:"HKD",
-        name:"홍콩 달러",
-        icon:"flags/hk.svg",
-        type:"currency"
-    },
-
-    {
-        code:"SGD",
-        name:"싱가포르 달러",
-        icon:"flags/sg.svg",
-        type:"currency"
-    },
-
-    {
-        code:"THB",
-        name:"태국 바트",
-        icon:"flags/th.svg",
-        type:"currency"
-    },
-
-    {
-        code:"VND",
-        name:"베트남 동",
-        icon:"flags/vn.svg",
-        type:"currency"
-    },
-
-    {
-        code:"TWD",
-        name:"대만 달러",
-        icon:"flags/tw.svg",
-        type:"currency"
-    },
-
-    {
-        code:"INR",
-        name:"인도 루피",
-        icon:"flags/in.svg",
-        type:"currency"
-    },
-
-    {
-        code:"IDR",
-        name:"인도네시아 루피아",
-        icon:"flags/id.svg",
-        type:"currency"
-    },
-
-    {
-        code:"MYR",
-        name:"말레이시아 링깃",
-        icon:"flags/my.svg",
-        type:"currency"
-    },
-
-    {
-        code:"PHP",
-        name:"필리핀 페소",
-        icon:"flags/ph.svg",
-        type:"currency"
-    },
-
-    {
-        code:"AED",
-        name:"아랍에미리트 디르함",
-        icon:"flags/ae.svg",
-        type:"currency"
-    },
-
-    {
-        code:"SAR",
-        name:"사우디 리얄",
-        icon:"flags/sa.svg",
-        type:"currency"
-    },
-
-    {
-        code:"TRY",
-        name:"터키 리라",
-        icon:"flags/tr.svg",
-        type:"currency"
-    },
-
-    {
-        code:"RUB",
-        name:"러시아 루블",
-        icon:"flags/ru.svg",
-        type:"currency"
-    },
-
-    {
-        code:"BRL",
-        name:"브라질 헤알",
-        icon:"flags/br.svg",
-        type:"currency"
-    },
+{
+    code:"KRW",
+    name:"대한민국 원",
+    icon:"flags/kr.svg",
+    type:"currency"
+},
 
 
-    {
-        code:"XAU",
-        name:"금 (1 트로이온스)",
-        icon:"metals/gold.svg",
-        type:"metal",
-        unit:"31.1035g",
-        description:"국제 금 시세 기준"
-    },
+{
+    code:"USD",
+    name:"미국 달러",
+    icon:"flags/us.svg",
+    type:"currency"
+},
 
-    {
-        code:"XAG",
-        name:"은 (1 트로이온스)",
-        icon:"metals/silver.svg",
-        type:"metal",
-        unit:"31.1035g",
-        description:"국제 은 시세 기준"
-    }
+
+{
+    code:"EUR",
+    name:"유로",
+    icon:"flags/eu.svg",
+    type:"currency"
+},
+
+
+{
+    code:"JPY",
+    name:"일본 엔",
+    icon:"flags/jp.svg",
+    type:"currency"
+},
+
+
+{
+    code:"CNY",
+    name:"중국 위안",
+    icon:"flags/cn.svg",
+    type:"currency"
+},
+
+
+{
+    code:"GBP",
+    name:"영국 파운드",
+    icon:"flags/gb.svg",
+    type:"currency"
+},
+
+
+{
+    code:"AUD",
+    name:"호주 달러",
+    icon:"flags/au.svg",
+    type:"currency"
+},
+
+
+{
+    code:"CAD",
+    name:"캐나다 달러",
+    icon:"flags/ca.svg",
+    type:"currency"
+},
+
+
+{
+    code:"CHF",
+    name:"스위스 프랑",
+    icon:"flags/ch.svg",
+    type:"currency"
+},
+
+
+{
+    code:"HKD",
+    name:"홍콩 달러",
+    icon:"flags/hk.svg",
+    type:"currency"
+},
+
+
+{
+    code:"SGD",
+    name:"싱가포르 달러",
+    icon:"flags/sg.svg",
+    type:"currency"
+},
+
+
+{
+    code:"THB",
+    name:"태국 바트",
+    icon:"flags/th.svg",
+    type:"currency"
+},
+
+
+{
+    code:"VND",
+    name:"베트남 동",
+    icon:"flags/vn.svg",
+    type:"currency"
+},
+
+
+{
+    code:"TWD",
+    name:"대만 달러",
+    icon:"flags/tw.svg",
+    type:"currency"
+},
+
+
+{
+    code:"INR",
+    name:"인도 루피",
+    icon:"flags/in.svg",
+    type:"currency"
+},
+
+
+{
+    code:"IDR",
+    name:"인도네시아 루피아",
+    icon:"flags/id.svg",
+    type:"currency"
+},
+
+
+{
+    code:"MYR",
+    name:"말레이시아 링깃",
+    icon:"flags/my.svg",
+    type:"currency"
+},
+
+
+{
+    code:"PHP",
+    name:"필리핀 페소",
+    icon:"flags/ph.svg",
+    type:"currency"
+},
+
+
+{
+    code:"AED",
+    name:"아랍에미리트 디르함",
+    icon:"flags/ae.svg",
+    type:"currency"
+},
+
+
+{
+    code:"SAR",
+    name:"사우디 리얄",
+    icon:"flags/sa.svg",
+    type:"currency"
+},
+
+
+{
+    code:"TRY",
+    name:"터키 리라",
+    icon:"flags/tr.svg",
+    type:"currency"
+},
+
+
+{
+    code:"RUB",
+    name:"러시아 루블",
+    icon:"flags/ru.svg",
+    type:"currency"
+},
+
+
+{
+    code:"BRL",
+    name:"브라질 헤알",
+    icon:"flags/br.svg",
+    type:"currency"
+},
+
+
+
+{
+    code:"XAU",
+    name:"금 (1 트로이온스)",
+    icon:"metals/gold.svg",
+    type:"metal",
+    unit:"31.1035g"
+},
+
+
+{
+    code:"XAG",
+    name:"은 (1 트로이온스)",
+    icon:"metals/silver.svg",
+    type:"metal",
+    unit:"31.1035g"
+}
+
 
 ];
